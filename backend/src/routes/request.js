a4ec782df19baa67e2a7aabb4ffea354cb49b8e3
Shanedra Singh <shanedrasingh89@gmail.com>
2025-05-22 00:53:16 +0530

    add file

--- backend/src/routes/request.js
+++ backend/src/routes/request.js
@@ -1,17 +1,38 @@
-const express = require('express');
-const { userAuth } = require("../middlewares/auth.js")
-
+const express = require("express");
+const { userAuth } = require("../middlewares/auth.js");
+const ConnectionRequest = require("../models/connectionRequest.js");
 
 const requestRouter = express.Router();
-requestRouter.post("/sendConnectionRequest",userAuth, async(req, res) => {
-    const user=req.user;
-  
-    // sending connetionn request
-  
-    console.log("Sending a connection request")
-  
-    res.send(user.firstName + "sent the connection request!")
-  });
 
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: `${req.user.firstName} sent the connection request successfully!`,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send({ error: err.message });
+    }
+  }
+);
 
 module.exports = requestRouter;
+
+ 
+
+
