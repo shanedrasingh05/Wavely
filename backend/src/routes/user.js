ff06d79b3369c7a680471887bd661faf7f1a4a57
Shanedra Singh <shanedrasingh89@gmail.com>
2025-06-08 02:38:58 +0530

    add file

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -3,13 +3,18 @@ const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_SAVE_DATA = "firstName lastName profileUrl age gender about skills";
+// photoUrl
+
+//Get all pending connection request for the loggedIn user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", ["firstName", "lastName"]);
+    }).populate("fromUserId", USER_SAVE_DATA);
+    // }).populate("fromUserId", ["firstName", "lastName", "photoUrl", "age", "about", "skills"]);
 
     res.json({
       message: "Data fetched successfully",
@@ -17,6 +22,42 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     });
   } catch (error) {
     res.status(400).send(error.message);
+  }
+});
+
+
+
+userRouter.get("/user/connections", userAuth, async (req,res)=>{
+  try{
+
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        // { toUserId: loggedInUser._id, status: "interested" },
+        // { fromUserId: loggedInUser._id, status: "interested" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAVE_DATA)
+      .populate("toUserId", USER_SAVE_DATA);
+
+    const data = connectionRequests.map((row) =>{
+      if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+          return row.toUserId;
+      }
+      return row.fromUserId;
+    })
+    
+  
+
+    res.json({data});
+    // res.json({ data: connectionRequests });
+
+  }
+  catch(err){
+        res.status(400).send({message: err.massage})
   }
 });
 
