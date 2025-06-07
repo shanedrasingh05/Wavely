7244efa5456166e63f425cb810d8f28d81f4763c
Shanedra Singh <shanedrasingh89@gmail.com>
2025-06-07 23:37:15 +0530

    add file

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,23 +1,23 @@
-// const express = require("express");
-// const { userAuth } = require("../middlewares/auth");
-// const userRouter = express.Router();
-// const ConnectionRequest = require("../models/connectionRequest");
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const userRouter = express.Router();
+const ConnectionRequest = require("../models/connectionRequest");
 
-// userRouter.get("/user/requests/received", userAuth, async (req, res) => {
-//   try {
-//     const loggedInUser = req.user;
-//     const connectionRequests = await ConnectionRequest.find({
-//       toUserId: loggedInUser._id,
-//       status: "interested",
-//     }).populate("fromUserId", ["firstName", "lastName"]);
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("fromUserId", ["firstName", "lastName"]);
 
-//     res.json({
-//       message: "Data fetched successfully",
-//       data: connectionRequests,
-//     });
-//   } catch (error) {
-//     res.status(400).send(error.message);
-//   }
-// });
+    res.json({
+      message: "Data fetched successfully",
+      data: connectionRequests,
+    });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
-// module.exports = userRouter;
+module.exports = userRouter;
