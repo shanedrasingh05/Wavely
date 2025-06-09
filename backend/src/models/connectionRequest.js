ee6d7f53e5b9c235f3f5fc98d5573b548b46883f
Shanedra Singh <shanedrasingh89@gmail.com>
2025-06-09 22:13:59 +0530

    add file

--- backend/src/models/connectionRequest.js
+++ backend/src/models/connectionRequest.js
@@ -1,49 +1,49 @@
-const mongoose= require('mongoose')
+// const mongoose= require('mongoose')
 
-const connectionRequestSchema = new mongoose.Schema({
+// const connectionRequestSchema = new mongoose.Schema({
     
-    fromUserId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",  /// Reference to the User collection
-        required: true,
-    },
+//     fromUserId:{
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "User",  /// Reference to the User collection
+//         required: true,
+//     },
 
-    toUserId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
+//     toUserId: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "User",
+//         required: true,
+//     },
 
-    status:{
-        type: String,
-        required: true,
-        enum:{
-            values:["ignored", "interested", "accepted", "rejected"],
-            message:"{VALUE} is not a valid status",
-    },
-},
+//     status:{
+//         type: String,
+//         required: true,
+//         enum:{
+//             values:["ignored", "interested", "accepted", "rejected"],
+//             message:"{VALUE} is not a valid status",
+//     },
+// },
 
-},
+// },
 
-{
-timestamps: true
-})
+// {
+// timestamps: true
+// })
 
-connectionRequestSchema.pre("save", function (next) {
-  const connectionRequest = this;
+// connectionRequestSchema.pre("save", function (next) {
+//   const connectionRequest = this;
 
-  // Check if the fromUserId is same as to userId
-  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
-    throw new Error("Cannot send connection request to yourself!");
-  }
-  next();
-});
-
-
-const ConnectionRequestModel = mongoose.model("ConnectionRequest", connectionRequestSchema)
-
-module.exports = ConnectionRequestModel
+//   // Check if the fromUserId is same as to userId
+//   if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+//     throw new Error("Cannot send connection request to yourself!");
+//   }
+//   next();
+// });
 
 
+// const ConnectionRequestModel = mongoose.model("ConnectionRequest", connectionRequestSchema)
 
-// module.exports = mongoose.model("ConnectionRequest", connectionRequestSchema)
+// module.exports = ConnectionRequestModel
+
+
+
+// // module.exports = mongoose.model("ConnectionRequest", connectionRequestSchema)
